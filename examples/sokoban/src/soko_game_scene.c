e4ffd0894f25f501976f854a33d21d7f76c7a309
C-Vieira <caiovieira.official@gmail.com>
2025-07-29 10:30:05 -0300

    feat(sokoban): brute force rock push mechanic
    Add crude pushing mechanic implementation with position checks.

--- examples/sokoban/src/soko_game_scene.c
+++ examples/sokoban/src/soko_game_scene.c
@@ -1,9 +1,13 @@
 #include "../soko.h"
+#include <curses.h>
 
 scene_data_t *gp_soko_game_scene;
 
 // Player reference
-entity_t s_player;
+static entity_t s_player;
+
+// Test rock entity
+static entity_t s_rock;
 
 // Views
 static view_data_t *p_game_view;
@@ -18,6 +22,12 @@ void soko_game_init() {
       (entity_t){.pos = {3, 8}, .ch = '@', .color = COLOR_PAIR(GREEN_BLACK)};
   // Draw it on the screen
   view_draw_entity_at(p_game_view, &s_player);
+
+  // Test rock entity
+  s_rock =
+      (entity_t){.pos = {5, 12}, .ch = 'O', .color = COLOR_PAIR(YELLOW_BLACK)};
+  // Draw it on the screen
+  view_draw_entity_at(p_game_view, &s_rock);
 
   view_draw(p_game_view);
 }
@@ -56,8 +66,26 @@ void soko_game_handle_input() {
 void soko_game_update() {
   vec2_t new_pos = vector_add(s_player.dir, s_player.pos);
 
+  bool player_is_colliding =
+      game_is_colliding_with_border(p_game_view, new_pos);
+  bool rock_is_colliding = false;
+
+  // Check for rock
+  if (view_get_char_at(p_game_view, new_pos.y, new_pos.x) ==
+      ('O' | COLOR_PAIR(YELLOW_BLACK))) {
+    vec2_t rock_new_pos = vector_add(s_player.dir, s_rock.pos);
+
+    rock_is_colliding =
+        game_is_colliding_with_border(p_game_view, rock_new_pos);
+    if (!rock_is_colliding) {
+      // Update rock position
+      s_rock.pos = rock_new_pos;
+    }
+  }
+
   // Check collisions
-  if (!game_is_colliding_with_border(p_game_view, new_pos)) {
+  if (!player_is_colliding && !rock_is_colliding) {
+
     // Clear trail
     vec2_t old_pos = s_player.pos;
     view_clear_char_at(p_game_view, old_pos.y, old_pos.x);
@@ -70,6 +98,9 @@ void soko_game_update() {
 void soko_game_draw() {
   // Draw player
   view_draw_entity_at(p_game_view, &s_player);
+
+  // Draw rock
+  view_draw_entity_at(p_game_view, &s_rock);
 
   view_draw(p_game_view);
 }
